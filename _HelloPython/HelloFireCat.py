f8478bee25f60cedf01d1bed792e8843bb4dd8b1
happydevcat <neo.rurouni@gmail.com>
2022-02-03 16:52:57 +0900

    Pyton 기본자료형

--- _HelloPython/HelloFireCat.py
+++ _HelloPython/HelloFireCat.py
@@ -1,3 +1,42 @@
-print("안녕 반가워 난 파이썬이야. ")
-print("Hello FireCat ~~ 덕배 ")
-print("난 집사지 ** ")
+#print("안녕 반가워 난 파이썬이야. ")
+#print("Hello FireCat ~~ 덕배 ")
+#print("난 집사지 ** ")
+
+"""
+_TempVar= 4
+_TempVar2= 3
+print(_TempVar//_TempVar2)
+print(type(_TempVar))
+
+
+_TempStr ='Python\'s favorite food is perl'
+_TempStr2 =' is Fun!!!!!'
+print((_TempStr+_TempStr2)[34])
+print((_TempStr+_TempStr2)[34:38])
+print((_TempStr+_TempStr2)[34:])
+print((_TempStr+_TempStr2)[0:38:-1])
+# 인덱싱(Indexing)
+# 슬라이싱(Slicing) 
+# 포멧팅(Formating)
+_TempNum = 10
+_TempStr = "Three"
+
+_TempStrJoin = "I ate %d apples, so I was sick for %s days." %(_TempNum, _TempStr)
+print(_TempStrJoin)
+
+_TempStr = "나는 {name}와 함께 {varDoit}를 했다".format(varDoit="식사", name="방구쟁이뿡뿡이")
+print(_TempStr)
+
+_TempStr2 = f"나의 이름은 {_TempNum} 입니다."
+print(_TempStr2)
+
+
+_TempStr = "Life is Lifes"
+print(_TempStr.replace("Life","Dogsound"))
+print(_TempStr.split())
+"""
+
+_TempArr = [1,2,3,4,5,6,7,8]
+_TempArr2 = [111,666,7,8]
+#del _TempArr2[2:4]
+print(_TempArr.count(7))
